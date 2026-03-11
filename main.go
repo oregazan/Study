7935e5f83b3c0e574c8f87ae90841f6e5180c797
t1ld <oregazan@gmail.com>
2026-03-11 22:13:31 +0300

    patch

--- main.go
+++ main.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"Study/postgres/simple_connection"
+	"Study/postgres/simple_sql"
+	"context"
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello Git")
-	simple_connection.CheckConnection()
+	ctx := context.Background()
+	conn, err := simple_connection.CreateConnection(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := simple_sql.CreateTable(ctx, conn); err != nil {
+		panic(err)
+	}
+	if err := simple_sql.InsertRow(
+		ctx,
+		conn,
+		"Обед2",
+		"Покушать",
+		false,
+		time.Now(),
+	); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Успешно!")
 }
--- postgres/simple_connection/simple_connection.go
+++ postgres/simple_connection/simple_connection.go
@@ -2,20 +2,10 @@ package simple_connection
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/jackc/pgx/v5"
 )
 
-func CheckConnection() {
-	ctx := context.Background()
-	conn, err := pgx.Connect(ctx, "postgres://postgres:admin@localhost:5432/postgres")
-	if err != nil {
-		panic(err)
-	}
-
-	if err := conn.Ping(ctx); err != nil {
-		panic(err)
-	}
-	fmt.Println("Подключение к базе данных произошло успешно!")
+func CreateConnection(ctx context.Context) (*pgx.Conn, error) {
+	return pgx.Connect(ctx, "postgres://postgres:admin@localhost:5432/postgres")
 }
